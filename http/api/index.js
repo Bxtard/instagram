65975e5a34f7297c54d6518e9c16027ab0e882f7
Bryan <ee.mxtrd@gmail.com>
2022-05-22 08:31:23 -0500

    inicializando usuario

--- http/api/index.js
+++ http/api/index.js
@@ -1,9 +1,12 @@
 
 const express = require("express");
+const users = require("../users/routes");
 const router = express.Router();
 
 router.get("/",(req, res) => {
     res.json("hello from api");
 });
+
+router.use("/users",users);
 
 module.exports = router;
---
+++ http/users/controller.js
@@ -0,0 +1,7 @@
+const users = require("./model");
+
+const list = (req, res) => {
+    res.json({data: users});
+};
+
+module.exports = {list};
