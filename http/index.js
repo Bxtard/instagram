61208407d27b104008a85bb862733d755182791a
Bryan <ee.mxtrd@gmail.com>
2022-05-22 08:01:51 -0500

    servidor configurado

--- http/index.js
+++ http/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const api = require("./api");
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json("hello from http");
 });
+
+app.use("/api", api);
+
 const init = () => {
     app.listen(3000, ()=>{
         console.log("server started");
